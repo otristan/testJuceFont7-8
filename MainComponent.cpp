41deecd326121ca41ae6616f65f209d3a1d78276
olivier tristan <o.tristan@uvi.net>
2025-01-16 08:38:54 +0100

    forgot those in previous commit

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -2,15 +2,31 @@
 
 #if USE_JUCE7
 #include "vf_FreeTypeFaces.h"
+#else
+
+class MyLookAndFeel : public juce::LookAndFeel_V4
+{
+  public:
+    juce::TypefaceMetricsKind getDefaultMetricsKind() const override
+    {
+      return juce::TypefaceMetricsKind::portable;
+    }
+};
+
+MyLookAndFeel myLookAndFeel;
+
 #endif
+
 
 //==============================================================================
 MainComponent::MainComponent()
 {
-  setSize (600, 400);
+  setSize (600, 800);
   
   addAndMakeVisible(&mLabel);
   addAndMakeVisible(&mLabel2);
+  addAndMakeVisible(&mLabel3);
+  addAndMakeVisible(&mLabel4);
   
 #if USE_JUCE7
   {
@@ -19,9 +35,10 @@ MainComponent::MainComponent()
     juce::Typeface::Ptr typeface = nullptr;
     if (iStream)
     {
-      typeface = new juce::CustomTypeface(*iStream);
-      auto font = juce::Font(typeface).withPointHeight(18.f);;
+      typeface = new juce::CustomTypeface(*iStream); // ascent = 0.798224389 FWIW
+      auto font = juce::Font(typeface).withHeight(28.f);
       mLabel.setFont(font);
+      mLabel2.setFont(font);
     }
   }
   {
@@ -32,21 +49,34 @@ MainComponent::MainComponent()
       auto block = std::make_shared<juce::MemoryBlock>();
       size_t fileSize = iStream->readIntoMemoryBlock(*block);
       FreeTypeFaces::addFaceFromMemory(file.getFullPathName(), 3.f, 5.f, true, block);
-      juce::Typeface::Ptr typeface = FreeTypeFaces::createTypefaceForFont(file.getFullPathName(), 34);
-      auto font = juce::Font(typeface).withPointHeight(34.f);
-      mLabel2.setFont(font);
+      juce::Typeface::Ptr typeface = FreeTypeFaces::createTypefaceForFont(file.getFullPathName(), 18.f);
+      mLabel3.setFont(juce::Font(typeface).withHeight(18.f));
+      
+      mLabel4.setFont(juce::Font(typeface).withHeight(30.f));
     }
   }
 #else
+  
+  juce::LookAndFeel::setDefaultLookAndFeel(&myLookAndFeel);
   {
-    juce::File file = juce::File(__FILE__).getSiblingFile("MyriadPro-Bold.ttf");
+    juce::File file = juce::File(__FILE__).getSiblingFile("MyriadPro-Bold-edited.ttf"); // kern pair removed, ascent and descent (898, 250 --> 898, 227 in typo and hhead) change to match jucefont ratio
     auto iStream = file.createInputStream();
     juce::MemoryBlock block;
     size_t fileSize = iStream->readIntoMemoryBlock(block);
     juce::Typeface::Ptr typeface = juce::Typeface::createSystemTypefaceFor(block.getData(), fileSize);
     
-    auto font = juce::Font(juce::FontOptions(typeface)).withPointHeight(18.f);
+    auto font = juce::Font(juce::FontOptions(typeface)).withHeight(28.f*0.9f);
     mLabel.setFont(font);
+  }
+  {
+    juce::File file = juce::File(__FILE__).getSiblingFile("MyriadPro-Bold-edited2.ttf"); // same as edited2 but with 0.9 scaling applied in type ascent/descent hence 997 252 (same ratio)
+    auto iStream = file.createInputStream();
+    juce::MemoryBlock block;
+    size_t fileSize = iStream->readIntoMemoryBlock(block);
+    juce::Typeface::Ptr typeface = juce::Typeface::createSystemTypefaceFor(block.getData(), fileSize);
+    
+    auto font = juce::Font(juce::FontOptions(typeface)).withHeight(28.f);
+    mLabel2.setFont(font);
   }
   {
     juce::File file = juce::File(__FILE__).getSiblingFile("DIN.ttf");
@@ -55,8 +85,18 @@ MainComponent::MainComponent()
     size_t fileSize = iStream->readIntoMemoryBlock(block);
     juce::Typeface::Ptr typeface = juce::Typeface::createSystemTypefaceFor(block.getData(), fileSize);
     
-    auto font = juce::Font(juce::FontOptions(typeface)).withPointHeight(34.f);
-    mLabel2.setFont(font);
+    auto font = juce::Font(juce::FontOptions(typeface)).withHeight(18.f*0.8f);
+    mLabel3.setFont(font);
+  }
+  {
+    juce::File file = juce::File(__FILE__).getSiblingFile("DIN-edited.ttf");
+    auto iStream = file.createInputStream();
+    juce::MemoryBlock block;
+    size_t fileSize = iStream->readIntoMemoryBlock(block);
+    juce::Typeface::Ptr typeface = juce::Typeface::createSystemTypefaceFor(block.getData(), fileSize);
+    
+    auto font = juce::Font(juce::FontOptions(typeface)).withHeight(30.f);
+    mLabel4.setFont(font);
   }
 #endif
   
@@ -65,10 +105,20 @@ MainComponent::MainComponent()
   mLabel.setColour(juce::Label::textColourId, juce::Colours::white);
   mLabel.setColour(juce::Label::backgroundColourId, juce::Colours::red);
   
-  mLabel2.setText("20000Hz", juce::dontSendNotification);
+  mLabel2.setText("Trigger Mode", juce::dontSendNotification);
   mLabel2.setJustificationType(juce::Justification::centred);
   mLabel2.setColour(juce::Label::textColourId, juce::Colours::white);
   mLabel2.setColour(juce::Label::backgroundColourId, juce::Colours::red);
+  
+  mLabel3.setText("20000Hz", juce::dontSendNotification);
+  mLabel3.setJustificationType(juce::Justification::centred);
+  mLabel3.setColour(juce::Label::textColourId, juce::Colours::white);
+  mLabel3.setColour(juce::Label::backgroundColourId, juce::Colours::red);
+  
+  mLabel4.setText("20000Hz", juce::dontSendNotification);
+  mLabel4.setJustificationType(juce::Justification::centred);
+  mLabel4.setColour(juce::Label::textColourId, juce::Colours::white);
+  mLabel4.setColour(juce::Label::backgroundColourId, juce::Colours::red);
 }
 
 //==============================================================================
@@ -79,8 +129,11 @@ void MainComponent::paint (juce::Graphics& g)
 
 void MainComponent::resized()
 {
-  mLabel.setSize(200, 50);
-  mLabel.setBoundsToFit(juce::Rectangle<int>(0, 0, getWidth(), getHeight()/2), juce::Justification::centred, true);
-  mLabel2.setSize(200, 50);
-  mLabel2.setBoundsToFit(juce::Rectangle<int>(0, getHeight()/2, getWidth(), getHeight()/2), juce::Justification::centred, true);
+  std::array<juce::Component*, 4> widgets = {&mLabel, &mLabel2, &mLabel3, &mLabel4};
+  
+  for (int i = 0; i < widgets.size(); i++)
+  {
+    widgets[i]->setSize(200, 50);
+    widgets[i]->setBoundsToFit(juce::Rectangle<int>(0, i*getHeight()/(float)widgets.size(), getWidth(), getHeight()/(float)widgets.size()), juce::Justification::centred, true);
+  }
 }
